c1c7a5821dbad8bf016b2a1f4c10ac962cfd7a2d
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2021-11-03 11:27:37 -0700

    Initial COmmit

---
+++ api_fantasy.py
@@ -0,0 +1,18 @@
+import pandas
+from sqlalchemy import create_engine
+import requests
+# Use df.to_sql and sqlalchemy https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+# Credentials to database connection
+#https://opentechguides.com/how-to/article/pandas/195/pandas-to-mysql.html
+hostname="147.182.207.57"
+dbname="fantasy_1"
+uname="pythoneverything"
+pwd="python123"
+data = requests.get('https://www.fantasyfootballdatapros.com/api/players/2019/all').json()
+df = pandas.json_normalize(data) #Normalize data to be flat
+#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.json_normalize.html#pandas.json_normalize
+print(df)
+# Create SQLAlchemy engine to connect to MySQL Database
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+            .format(host=hostname, db=dbname, user=uname, pw=pwd))
+df.to_sql('fantasy', engine, index=False, if_exists = 'replace')
