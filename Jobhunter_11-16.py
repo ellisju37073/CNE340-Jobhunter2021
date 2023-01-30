d4bb274989a791186a326253416251d64f8c5019
Justin Ellis <jellis@rtc.edu>
2023-01-30 13:09:19 -0800

    push 11.17.22

--- Jobhunter_11-16.py
+++ Jobhunter_11-16.py
@@ -17,7 +17,7 @@ def create_tables(cursor):
     # Must set Title to CHARSET utf8 unicode Source: http://mysql.rjweb.org/doc.php/charcoll.
     # Python is in latin-1 and error (Incorrect string value: '\xE2\x80\xAFAbi...') will occur if Description is not in unicode format due to the json data
     cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, Job_id varchar(50) , 
-    company varchar (300), Created_at DATE, url varchar(3000), Title LONGBLOB, Description LONGBLOB ); ''')
+    company LONGBLOB, Created_at DATE, url varchar(3000), Title LONGBLOB, Description LONGBLOB ); ''')
     return
 
 # Query the database.
